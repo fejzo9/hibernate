44a498d19d445368462e913070f3aa370747599c
fejzullahZ <fejzo999@gmail.com>
2022-08-16 13:56:37 +0200

    changed @RequestMapping(/api/car/model) into that, also putted method call directly into ResponseEntity

--- src/main/java/com/example/hibernate/controller/ModelController.java
+++ src/main/java/com/example/hibernate/controller/ModelController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/model")
+@RequestMapping("/api/car/model")
 public class ModelController {
 
     private final ModelService modelService;
@@ -20,8 +20,9 @@ public class ModelController {
     public ModelController(ModelService modelService) {
         this.modelService = modelService;
     }
+
     @GetMapping("")
-    public ResponseEntity<List<Model>> getAllModels(){
+    public ResponseEntity<List<Model>> getAllModels() {
         try {
             List<Model> models = modelService.getModels();
 
@@ -35,10 +36,9 @@ public class ModelController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Model> getModelById(@PathVariable("id") UUID id){
+    public ResponseEntity<Model> getModelById(@PathVariable("id") UUID id) {
         try {
-            Model model = modelService.getModelById(id);
-            return new ResponseEntity<>(model,HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(modelService.getModelById(id), HttpStatus.ACCEPTED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -46,7 +46,7 @@ public class ModelController {
 
     @PostMapping("")
     public ResponseEntity<Model> createModel(@RequestBody AddModel model) {
-        try{
+        try {
             return new ResponseEntity<>(modelService.addModel(model), HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -54,9 +54,9 @@ public class ModelController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Model> updateModel(@PathVariable("id") UUID id, @RequestBody Model model){
+    public ResponseEntity<Model> updateModel(@PathVariable("id") UUID id, @RequestBody Model model) {
         try {
-            return new ResponseEntity<>(modelService.updateModel(id,model), HttpStatus.OK);
+            return new ResponseEntity<>(modelService.updateModel(id, model), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -73,11 +73,10 @@ public class ModelController {
 
     @DeleteMapping("")
     public ResponseEntity<HttpStatus> deleteAllModels() {
-        try{
+        try {
             modelService.deleteAllModels();
             return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
