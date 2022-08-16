2087672f8d59d8e620698e1365152ab730f63b0a
fejzullahZ <fejzo999@gmail.com>
2022-08-16 12:15:57 +0200

    Adjust in CarService method deleteById

--- src/main/java/com/example/hibernate/service/ManufacturerService.java
+++ src/main/java/com/example/hibernate/service/ManufacturerService.java
@@ -3,6 +3,7 @@ package com.example.hibernate.service;
 import com.example.hibernate.model.AddManufacturer;
 import com.example.hibernate.model.Car;
 import com.example.hibernate.model.Manufacturer;
+import com.example.hibernate.model.Shop;
 import com.example.hibernate.repository.ManufacturerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,9 +49,13 @@ public class ManufacturerService {
         return manufacturer;
     }
 
-    public Manufacturer deleteById(UUID id) {
-        manufacturerRepository.deleteById(id);
-        return new Manufacturer();
+    public Manufacturer deleteById(UUID id) throws Exception {
+        Optional<Manufacturer> optionalManufacturer = manufacturerRepository.findById(id);
+
+        if (optionalManufacturer.isEmpty()) {
+            throw new Exception("error.../n The value is not present!/nDid not find the shop!");
+        } else{ manufacturerRepository.deleteById(id);
+            return optionalManufacturer.get();}
     }
 
     public void deleteAllManufacturers(){
